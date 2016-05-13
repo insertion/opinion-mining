42e3ddf516a4aaf17c2c7ad89abd06aa9ee078a3
wang lei <398758695@qq.com>
2016-05-13 22:18:01 +0800

    修复了一些bug

--- Spider/scrapy/getComment/getComment/settings.py
+++ Spider/scrapy/getComment/getComment/settings.py
@@ -10,9 +10,9 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'getComment'
-
 SPIDER_MODULES = ['getComment.spiders']
 NEWSPIDER_MODULE = 'getComment.spiders'
+DOWNLOADER_MIDDLEWARES = {'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : None,'getComment.UserAgentMiddle.RotateUserAgentMiddleware' :400}
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -24,7 +24,7 @@ NEWSPIDER_MODULE = 'getComment.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
--- Spider/scrapy/getComment/getComment/spiders/getcomment.py
+++ Spider/scrapy/getComment/getComment/spiders/getcomment.py
@@ -2,11 +2,13 @@
 import scrapy
 import json
 from  getComment.items import GetcommentItem
-
+import codecs
+#需要假如cookie，不然有些页面没有权限
 class GetcommentSpider(scrapy.Spider):
-    name = "getcomment"
+    name = "getComment"
     allowed_domains = ["douban.com"]
-    start_urls = []
+    start_urls = ['https://movie.douban.com/subject/26652816/comments?start=35&limit=20&sort=new_score']
+    '''
     def __init__(self):
         file=open('film_URL.json')
         for line in file.readlines():
@@ -14,18 +16,18 @@ class GetcommentSpider(scrapy.Spider):
             url=js['url']+'comments'
             self.start_urls.append(url)
         file.close()
-    
+    '''
     
     def parse(self, response):
         filmname=response.xpath('//*[@id="content"]/h1/text()').extract()[0]
         #extract()返回的是一个列表，里面第一个元素是unicode字符串
-        file=open(fillname,'wa')
+        file=codecs.open(filmname,'ab',encoding='utf-8')
         next=response.xpath('//*[@id="paginator"]/a[@class="next"]/@href').extract()
         item=GetcommentItem()
-        item('comment')=response.xpath('//*[@id="comments"]/div[@class="comment-item"]/div[2]/p/text()').extract()
-        item('title')  =response.xpath('//*[@id="comments"]/div[@class="comment-item"]/div[2]/h3/span[2]/span[1]/@title').extract()
+        item['comment']=response.xpath('//*[@id="comments"]/div[@class="comment-item"]/div[2]/p/text()').extract()
+        item['title']  =response.xpath('//*[@id="comments"]/div[@class="comment-item"]/div[2]/h3/span[2]/span[1]/@title').extract()
         commentlines = json.dumps(dict(item),ensure_ascii=False) + "\n"
         file.write(commentlines)
         if next:
-            next_url=request.url.split('?')[0]+next[0]
-            return scrapy.request(next_url,parse)
+            next_url=response.url.split('?')[0]+next[0]
+            return scrapy.Request(next_url,self.parse)
