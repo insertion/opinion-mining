b2164d34f48c23351bac26740fc5a1d5f0f9c763
wang lei <398758695@qq.com>
2016-05-14 14:30:21 +0800

    添加cookie，解决登录验证问题

--- Spider/scrapy/getComment/getComment/settings.py
+++ Spider/scrapy/getComment/getComment/settings.py
@@ -10,6 +10,7 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'getComment'
+DOWNLOAD_DELAY=3
 SPIDER_MODULES = ['getComment.spiders']
 NEWSPIDER_MODULE = 'getComment.spiders'
 DOWNLOADER_MIDDLEWARES = {'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : None,'getComment.UserAgentMiddle.RotateUserAgentMiddleware' :400}
@@ -24,7 +25,7 @@ DOWNLOADER_MIDDLEWARES = {'scrapy.downloadermiddlewares.useragent.UserAgentMiddl
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=3
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
--- Spider/scrapy/getComment/getComment/spiders/getcomment.py
+++ Spider/scrapy/getComment/getComment/spiders/getcomment.py
@@ -3,12 +3,38 @@ import scrapy
 import json
 from  getComment.items import GetcommentItem
 import codecs
-#需要假如cookie，不然有些页面没有权限
+#需要加入cookie，不然有些页面没有权限
 class GetcommentSpider(scrapy.Spider):
     name = "getComment"
     allowed_domains = ["douban.com"]
-    start_urls = ['https://movie.douban.com/subject/26652816/comments?start=35&limit=20&sort=new_score']
-    '''
+    cookie={ '__utma':"30149280.901747088.1445074673.1463148044.1463205092.69",
+             '__utma':"223695111.47263706.1446025707.1463148044.1463205092.27",
+             '__utmb':"30149280.0.10.1463205092",
+             '__utmb':"223695111.0.10.1463205092",
+             '__utmc':"30149280",
+             '__utmc':"223695111",
+             '__utmv':"30149280.13938",
+             '__utmz':"30149280.1463051064.63.51.utmcsr=baidu|utmccn=(organic)|utmcmd=organic",
+             '__utmz':"223695111.1463035423.19.13.utmcsr=baidu|utmccn=(organic)|utmcmd=organic",
+             '_pk_id.100001.4cf6':"54f6d2f316960e51.1446025708.27.1463205379.1463148922.",
+             '_pk_ref.100001.4cf6':'["","",1463204969,"http://www.baidu.com/link?url=YQLEs5QV1zmk47dXRps0dqtoMVwYwRFUN5-N9639eoU21p9BFeaxhNRstgUq9Vvs&wd=&eqid=f68d50f40003ae9a000000035734261a"]',
+             '_pk_ses.100001.4cf6':"*",
+             'ap':"1",
+             'bid':'"8P5Iz4n5Ws8"',
+             'ck':"8vtY",
+             'ct':"y",
+             'dbcl2':'"59034306:TCI0yjpqBT4"',
+             'gr_user_id':"8121958b-b647-4f44-bc4a-6ce28baf2d5d",
+             'll':'"118163"',
+             'ps':"y",
+             'push_doumail_num':"38",
+             'push_noty_num':"6",
+             'ue':'"398758695@qq.com"',
+             'viewed':'"1756954_1052241_1831698_25952655_1231361_7906768_24703171_3288908_2305237_6510682"',
+    }
+    #cookie是个字典
+    start_urls = []
+    
     def __init__(self):
         file=open('film_URL.json')
         for line in file.readlines():
@@ -16,10 +42,9 @@ class GetcommentSpider(scrapy.Spider):
             url=js['url']+'comments'
             self.start_urls.append(url)
         file.close()
-    '''
     
     def parse(self, response):
-        filmname=response.xpath('//*[@id="content"]/h1/text()').extract()[0]
+        filmname=response.xpath('//*[@id="content"]/h1/text()').extract()[0]+'.json'
         #extract()返回的是一个列表，里面第一个元素是unicode字符串
         file=codecs.open(filmname,'ab',encoding='utf-8')
         next=response.xpath('//*[@id="paginator"]/a[@class="next"]/@href').extract()
@@ -30,4 +55,4 @@ class GetcommentSpider(scrapy.Spider):
         file.write(commentlines)
         if next:
             next_url=response.url.split('?')[0]+next[0]
-            return scrapy.Request(next_url,self.parse)
+            return scrapy.Request(next_url,self.parse,cookies=self.cookie)
